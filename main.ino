da971110248a1102a4c1664262722477df28cb64
fakuu <tymkiwfacundo04@gmail.com>
2022-08-26 00:48:39 -0300

    boton

--- main.ino
+++ main.ino
@@ -1,61 +1,39 @@
-class Motor
-{
-    //atributos
-private:
-    int velocidad = 200;
-    
-    int pin_direccion;
-    int pin_pwm;
-    const int freq = 5000;
-    const int channel = 1;
-    const int bits = 8;
-    bool invert;
-public:
+class Button {
+  private:
+    int pin = 9
+    bool state = HIGH;
 
-    Motor(int dir, int pwm){
-        pin_direccion = dir;
-        pin_pwm = pwm;
+    //metodo
+  public:
+    buttonPin(int p) {
+      pin = p;
 
-        pinMode(pin_direccion, OUTPUT);
+      pinMode(pin, INPUT);
+    }
 
-    	ledcSetup(channel, freq, bits);
-	    ledcAttachPin(pin_pwm, channel);
+    //metodos o acciones
+    bool getIsPress() {
+      bool estado = digitalRead(pin);
+      return estado;
     }
-//metodos
-    void Forward (){
-        digitalWrite(pin_direccion, invert);
-        ledcWrite(pin_pwm, velocidad);
+
+    String getIsPressText() {
+      if (getIsPress()) return "PRESS";
+      else return "IS NOT PRESS";
     }
-    void Backward (){
-        digitalWrite(pin_direccion, !invert);
-        ledcWrite(pin_pwm, velocidad);
-    }
-    void invertDirection()
-    {
-        //cambio el valor del flag invertir.
-        invert = !invert;
-    }
-    void Stop (){
-        digitalWrite(pin_direccion, LOW );
-        ledcWrite(pin_pwm, 0);
-    }        
+
 };
 
-// motores
-#define MR1 18
-#define MR2 5
-#define ML1 4
-#define ML2 2
+#define BUTTON 4  //te ponen 
 
-//Instancio los motores
-Motor m1 = Motor(MR1, MR2);
-Motor m2 = Motor(ML1, ML2);
+Button *strategy = new buttonPin(BUTTON);
 
-void setup()
-{
+//holis pepito
+void setup(){
+
+}
+void loop(){
     
 }
-void loop()
-{
-    
-}
+
+
